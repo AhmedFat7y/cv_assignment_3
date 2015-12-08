f1f9a5220e527dff7d6ea9eb30740e9346f77455
Ahmed Abdel-wahab <ahmed.abdel-wahab@robustastudio.com>
2015-12-08 19:22:11 +0200

    initiali commit

---
+++ assignment-3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  assignment-3
+//
+//  Created by MacBook Air on 12/8/15.
+//  Copyright (c) 2015 Unlimited Blade Works. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
